dcb5ebdc0a8e6306aa92bca2b1567c8675308360
Viktor Bondar <viktorbo108@gmail.com>
2015-11-21 23:03:10 +0200

    work 3.1 ver 8

--- polls/models (Конфликтующая копия с компьютера bvs-ПК 2015-11-21).py
+++ polls/models (Конфликтующая копия с компьютера bvs-ПК 2015-11-21).py
@@ -1,14 +1,15 @@
-import datetime
-from django.db import models
-from django.utils import timezone
+import datetime 
+from django.db import models 
+from django.utils import timezone 
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self): # __unicode__ on Python 2
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
@@ -20,5 +21,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self): # __unicode__ on Python 2
         return self.choice_text    
