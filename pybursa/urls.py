7a08b051900c84c009db9a63f059642e0afee31b
Viktor Bondar <viktorbo108@gmail.com>
2015-11-28 22:37:34 +0200

    work 5.1 ver 4

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,11 +8,12 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^quadratic/', include('quadratic.urls', namespace="quadratic")),
-    url(r'^$', views.index, name='index2'),
+    url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^student_list/$', views.student_list, name='student_list'),
     url(r'^student_detail/$', views.student_detail, name='student_detail'),
     url(r'^courses/', include('courses.urls', namespace="courses")),
+    url(r'^students/', include('students.urls', namespace="students")),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from students.models import Student
 
-# Create your views here.
+
+def list_view(request):
+    reguest_course = request.GET
+    if 'course_id' in reguest_course:
+        list_students = Student.objects.filter(
+            courses=reguest_course['course_id'])
+    else:
+        list_students = Student.objects.all()
+    return render(request, 'students/list.html', {'list_students': list_students})
+
+
+def detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    return render(request, 'students/detail.html', {'student': student})
